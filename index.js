bff8a175b141bad7357a9beca12f73b1b9bda0cb
vidhilohia16 <vidhilohia16@gmail.com>
2026-06-02 02:28:11 +0530

    react project render

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ const path = require("path");
 
 app.use(express.static(path.join(__dirname, "vite-project/build")));
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "vite-project", "build", "index.html"));
 });
 app.use(express.json());
