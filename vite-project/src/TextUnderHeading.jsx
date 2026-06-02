5062174e2c958cf3b3560889a02638bb782cef5f
vidhilohia16 <vidhilohia16@gmail.com>
2026-06-03 04:34:37 +0530

    scroll to top

--- vite-project/src/TextUnderHeading.jsx
+++ vite-project/src/TextUnderHeading.jsx
@@ -1,10 +1,13 @@
 import Typography from '@mui/material/Typography';
 export default function TextUnderHeading(){
-   return <Typography variant="h5" sx={{
+   return <Box sx={{mx:"auto"}}>
+      <Typography variant="h5" sx={{
     textAlign:"center",
     color:"grey",
     mt:"20px"
+
    }}>
     Upload your contacts spreadsheet, compose your pitch, attach your resume — and blast it to hundreds of recruiters at once
    </Typography>
+   </Box>
 }
