064351abfc31c880205a400d5aa5ef7a4b025e67
vidhilohia16 <vidhilohia16@gmail.com>
2026-06-03 23:03:54 +0530

    mobile submit

--- vite-project/src/FakeButtons.jsx
+++ vite-project/src/FakeButtons.jsx
@@ -14,7 +14,7 @@ export default function FakeButtons(){
           <Box sx={{display:"flex",width:{xs:"100%",md:"600px"},gap:"0px",alignItems:"center"}}>
             
         <Button variant="contained" disabled sx={{borderRadius: "999px","&.Mui-disabled": {
-            backgroundColor: "#58585d",
+            backgroundColor: "#1111c6",
             fontSize:{xs:"10px",md:"14px"},
             padding:{xs:"4px 8px",md:"6px 16px"},
             flexShrink:0,
